413c26c0aad49a38f5647b79564f68a731283601
Jay Rauniar Sood <jay.sood98@gmail.com>
2022-09-03 18:31:43 +0100

    Found bounding box for both portrait and landscape timings, added greyscale conversion

--- ocr.py
+++ ocr.py
@@ -2,5 +2,7 @@ from PIL import Image, ImageGrab
 import sys
 import pytesseract
 
-file = sys.argv[1]
-print(pytesseract.image_to_string(Image.open(file)))
+file = "latest.jpg"
+
+content = pytesseract.image_to_string(Image.open(file).convert('L'))
+print(content)
--- screenshot.py
+++ screenshot.py
@@ -1,14 +1,17 @@
 from PIL import ImageGrab
 import time
-
 while True:
     start_time = time.time()
-    screenshot = ImageGrab.grab(bbox=(0, 0, 3000, 2100))
+
+    #screenshot = ImageGrab.grab(bbox=(0, 400, 880, 1950)) #portrait
+    screenshot = ImageGrab.grab(bbox=(0, 250, 2600, 1610))
     screenshot = screenshot.convert("RGB")
     screenshot.save("latest.jpg", "jpeg")
     print("Screenshot taken")
+
     end_time = time.time()
     time_between_shots = end_time - start_time
     print("Time between screenshots: " + str(time_between_shots))
-    time.sleep(1)
+
+    #time.sleep(1)
 
