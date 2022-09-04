babfde21fd477bdb3e4e503d9ea3a5c929b51799
Jay Rauniar Sood <jay.sood98@gmail.com>
2022-09-04 23:34:24 +0100

    started using table ocr library to parse screenshot input

--- ocr.py
+++ ocr.py
@@ -1,8 +1,16 @@
 from PIL import Image, ImageGrab
 import sys
 import pytesseract
+import table_ocr.util
+import table_ocr.extract_tables
+import table_ocr.extract_cells
+import table_ocr.ocr_image
+import table_ocr.ocr_to_csv
 
 file = "latest.jpg"
 
 content = pytesseract.image_to_string(Image.open(file).convert('L'))
 print(content)
+
+kart_table = table_ocr.extract_tables.main([file])
+print(kart_table)
